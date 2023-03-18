b78d8337312d246ca217911f91610cb536663536
LuchoLorenzo1 <lucianolorenzo1@hotmail.com>
2023-03-18 15:59:16 -0300

    adding cors

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './config.js'
 import express from 'express'
 import notes from './routes/notes.routes.js'
 import accounts from './routes/accounts.routes.js'
+import cors from 'cors'
 
 const app = express()
 import './databaseConnection.js'
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
